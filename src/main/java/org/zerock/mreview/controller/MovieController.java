6da07e2bdff35247ab8134098e88e629a40f4abd
lyj <lyj@diquest.com>
2022-11-01 21:17:53 +0900

    REST 변경 자업 중간 commit

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -2,12 +2,14 @@ package org.zerock.mreview.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.mreview.dto.MovieDTO;
 import org.zerock.mreview.dto.PageRequestDTO;
@@ -31,7 +33,7 @@ public class MovieController {
 
   }
 
-  @PostMapping("/register")
+  /*@PostMapping("/register")
   public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
 
     log.info("movieDTO:" + movieDTO);
@@ -41,22 +43,17 @@ public class MovieController {
     redirectAttributes.addFlashAttribute("msg", mno);
 
     return "redirect:/movie/list";
-  }
+  }*/
 
   @GetMapping("/list")
   public void list(PageRequestDTO pageRequestDTO, Model model){
-    log.info("pageRequestDTO:" + pageRequestDTO);
-
-    model.addAttribute("result", movieService.getList(pageRequestDTO));
+    model.addAttribute("page", pageRequestDTO);
   }
 
   @GetMapping({"/read", "/modify"})
-  public void read(long mno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+  public void read(long mno, Model model){
     log.info("mno:" + mno);
-
-    MovieDTO movieDTO = movieService.getMovie(mno);
-
-    model.addAttribute("dto", movieDTO);
+    model.addAttribute("mno", mno);
   }
 
   @PostMapping("/remove")
---
+++ src/main/java/org/zerock/mreview/controller/MovieRestController.java
@@ -0,0 +1,52 @@
+package org.zerock.mreview.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.dto.PageRequestDTO;
+import org.zerock.mreview.dto.PageResultDTO;
+import org.zerock.mreview.service.MovieService;
+
+@RestController
+@RequestMapping("/movies")
+@Log4j2
+@RequiredArgsConstructor
+public class MovieRestController {
+
+    private final MovieService movieService;
+
+    @GetMapping("")
+    public ResponseEntity<PageResultDTO> list(PageRequestDTO requestDTO) {
+        return new ResponseEntity<PageResultDTO>(movieService.getList(requestDTO), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(MovieDTO movieDTO) {
+        log.info("movieDTO:" + movieDTO);
+        Long mno = movieService.register(movieDTO);
+        return new ResponseEntity<Long>(mno, HttpStatus.OK);
+    }
+
+    @GetMapping("/{mno}")
+    public ResponseEntity<MovieDTO> get(@PathVariable long mno) {
+        return new ResponseEntity<MovieDTO>(movieService.getMovie(mno), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{mno}")
+    public ResponseEntity<Long> remove(long mno) {
+
+        log.info("mno:" + mno);
+        movieService.remove(mno);
+
+        return new ResponseEntity(mno, HttpStatus.OK);
+    }
+
+
+}
