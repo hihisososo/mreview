62c8d04364bf40fde78c03c3631ee82c045000aa
lyj <lyj@diquest.com>
2022-11-12 15:25:02 +0900

    part05 소셜 로그인

--- src/main/java/org/zerock/mreview/config/SecurityConfig.java
+++ src/main/java/org/zerock/mreview/config/SecurityConfig.java
@@ -1,24 +1,25 @@
 package org.zerock.mreview.config;
 
-import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zerock.mreview.security.dto.ClubLoginSuccessHandler;
+import org.zerock.mreview.security.service.ClubOAuth2UserDetailsService;
+import org.zerock.mreview.security.service.ClubUserDetailService;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private ClubUserDetailService userDetailsService;
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -28,14 +29,22 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
+                .antMatchers("/h2-console/**").permitAll();
+              /*  .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER")
+                .antMatchers("/sample/admin").hasRole("ADMIN");*/
         http.headers().frameOptions().disable();
         http.csrf().disable();
-        http.formLogin(); //인증, 인가 실패할 경우 로그인 화면
+        http.formLogin()//인증, 인가 실패할 경우 로그인 화면
+                .successHandler(new ClubLoginSuccessHandler(passwordEncoder()));
+        http.oauth2Login()//oauth2 로그인 설정
+                .successHandler(new ClubLoginSuccessHandler(passwordEncoder()));
         http.logout(); //로그아웃 설정
 
+        http.rememberMe().tokenValiditySeconds(60*60*24*7)
+                .userDetailsService(userDetailsService);
         return http.build();
     }
+
+
 }
--- src/main/java/org/zerock/mreview/controller/SampleController.java
+++ src/main/java/org/zerock/mreview/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +12,32 @@ import org.zerock.mreview.security.dto.ClubAuthMemberDTO;
 @Log4j2
 @RequestMapping("/sample")
 public class SampleController {
-  @GetMapping("/all")
-  public void exAll(){
-    log.info("exAll....");
-  }
+    @GetMapping("/all")
+    public void exAll() {
+        log.info("exAll....");
+    }
 
-  @GetMapping("/member")
-  public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
-    log.info("exMember....");
+    @GetMapping("/member")
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember) {
+        log.info("exMember....");
 
-    log.info("------------");
-    log.info(clubAuthMember);
+        log.info("------------");
+        log.info(clubAuthMember);
 
-  }
+    }
 
-  @GetMapping("/admin")
-  public void exAdmin(){
-    log.info("exAdmin....");
-  }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public void exAdmin() {
+        log.info("exAdmin....");
+    }
+
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@zerock.org\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember) {
+        log.info("exMemberOnly....");
+        log.info(clubAuthMember);
+        return "/sample/admin";
+
+    }
 }
